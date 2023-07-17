01dfbd27f3124e59d1948a982861600426a91acf
Alexandre <alexandrefaria3012@gmail.com>
2023-07-17 16:28:24 -0300

    Update contact_page.dart

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 
 class ContactPage extends StatefulWidget {
-  const ContactPage({Key? key, required this.contact}) : super(key: key);
+  //const ContactPage({Key? key, required this.contact}) : super(key: key);
 
   final Contact contact;
 
@@ -13,6 +13,21 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+
+  Contact? _editContact;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    if(widget.contact == null){
+      _editContact = Contact();
+    }else{
+      _editContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
