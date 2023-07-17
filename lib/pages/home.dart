cb5c68bac6f73112a032844eee90e25487a1dc19
Alexandre <alexandrefaria3012@gmail.com>
2023-07-17 16:13:38 -0300

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -27,20 +27,20 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         appBar: AppBar(title:
         Text('Contatos'),
-          backgroundColor(Colors.red),
+          backgroundColor: Colors.red,
           centerTitle: true,
-        )
-        backgroundColor(Colors.white),
+        ),
+      backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
       onPressed: (){},
+      backgroundColor:Colors.red,
       child: Icon(Icons.add),
-      backgroundColor(Colors.red),
       ),
       body: ListView.builder(
-        padding: EdgeInsetsAll(10.0),
+        padding: const EdgeInsets.all(10.0),
         itemCount: contatcs.length,
         itemBuilder: (context, index){
 
@@ -48,4 +48,43 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+
+  Widget contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null ? FileImage(File(contacts[index].img)):
+                        AssetImage('images/person.png')
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  Text(contacts[index].name ?? '',
+                    style: TextStyle(fontSize: 22.0,
+                    fontWeight: FontWeight.bold),
+                  ),
+                  Text(contacts[index].email ?? '',
+                    style: TextStyle(fontSize: 18.0,),
+                ),
+                  Text(contacts[index].phone ?? '',
+                    style: TextStyle(fontSize: 18.0,),
+              ),
+            ],
+          )
+        ),
+      )
+    );
+  }
+
 }
